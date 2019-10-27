8e258dc020864544df7f0895cfb4536c4ad0286b
lorikhamala <lori@codethedream.org>
2019-10-27 14:25:23 +0000

    reorg

--- test_2/catsdogs.rb
+++ test_2/catsdogs.rb
@@ -3,4 +3,4 @@ dogs = gets.chomp
 puts "How many cats do you have?"
 cats= gets.chomp
 pets = dogs.to_i + cats.to_i
-puts "So, you have #{dogs} dogs and #{cats} cats. You have " + pets.to_s + " pets all together."
+puts "So, you have #{dogs} dogs and #{cats} cats. You have " + pets.to_s + " pets all together.
