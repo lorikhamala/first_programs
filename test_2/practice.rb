450d157665b8f281a2c44f454dd8297abb21f165
lorikhamala <lori@codethedream.org>
2019-10-27 14:51:47 +0000

    file for oh a test

---
+++ test_2/practice.rb
@@ -0,0 +1,15 @@
+puts "This is a test."
+answer = gets.chomp
+if answer == "a what" or "a what?"
+    puts "A test."
+else puts "You're supposed to write 'A test!'"
+end
+answer = gets.chomp
+if answer == "a what" or "a what?"
+    puts "A test."
+    puts "Oh, a test."
+else puts "You're supposed to write 'A test!'"
+end
+puts "Oh, a test!"
+
+
